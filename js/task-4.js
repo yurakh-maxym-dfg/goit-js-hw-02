1f5676de847b315a409ba0d9cfdc50872a1e512b
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-03-01 20:59:22 +0200

    task-4 done

---
+++ js/task-4.js
@@ -0,0 +1,26 @@
+function getShippingCost(country){
+    const lowerCountry = country.toLowerCase();
+    switch(lowerCountry){
+    case "china":
+        return `Shipping to ${country} will cost 100 credits`;
+        break;
+    case "chile":
+        return `Shipping to ${country} will cost 250 credits`;
+        break;
+    case "australia":
+        return `Shipping to ${country} will cost 170 credits`;
+        break;
+    case "jamaica":
+        return `Shipping to ${country} will cost 120 credits`;
+        break;
+    default:
+        return "Sorry, there is no delivery to your country";
+    }   
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
